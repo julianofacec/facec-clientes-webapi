5113f1537c77a91a349796b385e7a78fb9a7f03d
Juliano Maciel <julianoleicam@gmail.com>
2022-05-19 09:30:35 -0300

    Adicionado cors ao projeto

--- Facec.WebApi/Startup.cs
+++ Facec.WebApi/Startup.cs
@@ -44,6 +44,8 @@ namespace Facec.WebApi
             });
 
             services.AddControllers();
+            services.AddCors();
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Facec.WebApi", Version = "v1" });
